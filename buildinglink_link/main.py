cf34437e9ace57765b7b0d4e415e423e3aec98e0
dclayh <dclayh@gmail.com>
2020-11-04 17:25:27 -0500

    Make waiver optional; handle empty timeslot

--- buildinglink_link/main.py
+++ buildinglink_link/main.py
@@ -12,6 +12,7 @@ import yaml
 BASE_URL = "https://www.buildinglink.com/V2"
 RESERVE_PAGE = "Tenant/Amenities/NewReservation.aspx"
 NAME_MAP = {"Clay": "David Hambrick", "Semmie": "Semmie Kim"}
+AMENITY_MAP = {"": "[12th Floor] Fitness Center (", "pool": "[12th Floor] Saltwater Lap Pool ("}
 
 
 def load_page(driver, page):
@@ -36,8 +37,12 @@ def next_day_schedule(today):
     schedule = yaml.load(resource_stream("buildinglink_link", "config/schedule.yaml"))
     tomorrow_day_of_week = (today.date() + timedelta(days=1)).strftime("%A")
     tomorrow_schedule = defaultdict(list)
-    for person, timeslot in schedule.get(tomorrow_day_of_week, {}).items():
-        tomorrow_schedule[timeslot].append(NAME_MAP[person])
+    for person, timeslot_amenity in schedule.get(tomorrow_day_of_week, {}).items():
+        if not timeslot_amenity:
+            continue
+        tsa_list = timeslot_amenity.split() + [""]
+        amenity_timeslot_str = AMENITY_MAP[tsa_list[1]] + tsa_list[0]
+        tomorrow_schedule[amenity_timeslot_str].append(NAME_MAP[person])
     return tomorrow_schedule.items()
 
 
@@ -46,10 +51,10 @@ def main():
     driver = webdriver.Firefox()
     driver.implicitly_wait(10)
     tomorrow_str = (datetime.now() + timedelta(days=1)).strftime("%A, %B %d, %Y")
-    for timeslot, people in next_day_schedule(datetime.now()):
-        logging.info("Registering %s for %s on %s.", people, timeslot, tomorrow_str)
+    for amenity_timeslot, people in next_day_schedule(datetime.now()):
+        logging.info("Registering %s for %s on %s.", people, amenity_timeslot + ")", tomorrow_str)
         load_page(driver, f"{BASE_URL}/{RESERVE_PAGE}")
-        driver.find_element_by_partial_link_text(f"[12th Floor] Fitness Center ({timeslot}").click()
+        driver.find_element_by_partial_link_text(amenity_timeslot).click()
         occupant_dropdown = driver.find_element_by_id("ctl00_ContentPlaceHolder1_OccupantList")
         occupant_dropdown.click()
         for resident in driver.find_element_by_id("ctl00_ContentPlaceHolder1_OccupantList_DropDown").find_elements_by_tag_name("label"):
@@ -59,7 +64,10 @@ def main():
         occupant_dropdown.click()
         time.sleep(2)
         driver.find_element_by_xpath(f"//td[@title='{tomorrow_str}']").click()
-        driver.find_element_by_id("ctl00_ContentPlaceHolder1_liabilityWaiverAgreeTextbox").send_keys("Yes")
+        try:
+            driver.find_element_by_id("ctl00_ContentPlaceHolder1_liabilityWaiverAgreeTextbox").send_keys("Yes")
+        except NoSuchElementException:
+            pass
         time.sleep(2)
         save_button = driver.find_element_by_id("ctl00_ContentPlaceHolder1_FooterSaveButton")
         save_button.find_element_by_tag_name("span").click()
